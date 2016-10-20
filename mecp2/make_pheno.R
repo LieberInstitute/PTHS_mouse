ca4ceb253e1ce4d3c074ec3c85741b52ca2a40ff
badoi <badoi@me.com>
2016-10-19 23:36:24 -0400

    add pten, mecp2, and sweatt pheno.rda

--- mecp2/make_pheno.R
+++ mecp2/make_pheno.R
@@ -4,7 +4,7 @@ library(jaffelab)
 dir = "/dcl01/lieber/ajaffe/Brady/mouseRNAseq/Rett"
 pd = read.csv("./tables/SraRunInfo.csv",stringsAsFactors = FALSE)
 pd$Genotype = factor(pd$Geno, levels=c("wild type", "MeCP2 KO"))
-save(pd,file = './tables/pheno.rda')
+save(pd,file = './rdas/pheno.rda')
 
 
 write.table(pd$SampleID,file = paste0(dir,'/SAMPLE_IDs.txt'),col.names = F,row.names = F,quote = F)
--- pten/make_pheno.R
+++ pten/make_pheno.R
@@ -24,7 +24,7 @@ pd = data.frame(SampleID = paste0("Mouse", seq(along=split(reads,sampleID))),
                 FileID = rownames(bySample), stringsAsFactors=FALSE)
 pd$Genotype = factor(t$genotype_s,levels = c("Pten wt/wt",'Pten wt/m3m4','Pten m3m4/m3m4'))
 pd$Age = factor(t$age_s,levels = c('2-weeks old at sacrifice','6-weeks old at sacrifice'))
-save(pd,file = './tables/pheno.rda')
+save(pd,file = './rdas/pheno.rda')
 
 write.table(pd$FileID,file = "/dcl01/lieber/ajaffe/Brady/mouseRNAseq/TilotPTEN/SAMPLE_IDs.txt",
             row.names = F,col.names = F,quote = F)
