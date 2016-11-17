8181c6068e26eb5f66d5e238154eda7b1f0fd242
badoi <badoi@me.com>
2016-11-17 11:28:42 -0500

    changed sfig1 to mTCF4 transcript 1

--- fig1_plot_tcf4_exons.R
+++ fig1_plot_tcf4_exons.R
@@ -1,5 +1,5 @@
 ## plot TCF4 Exon fold-change in genomic location order
-## plots last exon of ENSMUST00000114982 tcf4 transcript to show timeline expression
+## plots last exon of ENSMUST00000114985 tcf4 transcript to show timeline expression
 cleaningY = function(y, mod, P=ncol(mod)) {
   Hat=solve(t(mod)%*%mod)%*%t(mod)
   beta=(Hat%*%t(y))
@@ -16,6 +16,24 @@ library(DESeq2)
 load( '/dcl01/lieber/ajaffe/Brady/mouseRNAseq/mouse_tcf4_DESeq2_svaAdj.rda')
 load("tcf4_mouse/rdas/mouse_tcf4_DE_objects_DESeq2.rda")
 
+######################################
+# summary of TCF4 expression patterns
+outGene[outGene$Symbol=='Tcf4',]
+tcf4Index = which(outExon$Symbol=='Tcf4' & 
+                    outExon$padj< 0.05 & 
+                    !is.na(outExon$padj) &
+                    outExon$log2FoldChange <0)
+apply(outExon[tcf4Index,1:6],2,summary)
+
+#################
+# summary of DEGs
+gIndex = which(outGene$Symbol!='Tcf4' & 
+                    outGene$padj< 0.05 & 
+                    !is.na(outGene$padj))
+sigGene = outGene[gIndex,]
+dim(sigGene) #42 DEGs aside from TCF4
+table(sign(sigGene$log2FoldChange)) # 21 up, 21 down
+
 #################
 # get rlog counts
 yExon = rlog(exonDds)
@@ -26,7 +44,7 @@ yExon = assays(yExon)[[1]]
 ensembl = useMart("ensembl")
 ensembl = useDataset("mmusculus_gene_ensembl",mart=ensembl)
 sym = getBM(attributes = c('chromosome_name','exon_chrom_start','exon_chrom_end',"ensembl_transcript_id"), 
-            mart=ensembl,filters = 'ensembl_transcript_id',value ='ENSMUST00000114982')
+            mart=ensembl,filters = 'ensembl_transcript_id',value ='ENSMUST00000114985')
 
 #####################
 # get only Tcf4 exons
@@ -59,7 +77,7 @@ pdf("tcf4_mouse/plots/sfig1_tcf4_transcript_2_exons.pdf",height = 5, width = 6)
 ylim = c(min(outExon2[,c('CI.L','CI.R')])-.2,max(outExon2[,c('CI.L','CI.R')])+.2)
 outExon2$col = ifelse(outExon2$adj.P.Val<.05,'red' ,'black')
 plot(outExon2$pos,outExon2$logFC,xlab = 'Exon #',ylab = 'Log2 Fold-change',
-     ylim = ylim,main = 'Mouse Tcf4 Transcript 2 Fold-change',cex.lab = 1.25,bg = outExon2$col,pch = 21)
+     ylim = ylim,main = 'Tcf4 Transcript 1 Exon Fold-change',cex.lab = 1.25,bg = outExon2$col,pch = 21)
 arrows(as.numeric(outExon2$pos),outExon2$CI.L,as.numeric(outExon2$pos),outExon2$CI.R,
        code=3,length=0.04,angle=90,col='black')
 v1 = seq(1,nrow(outExon2),4)
@@ -69,20 +87,21 @@ text(x = ind,y= outExon2$CI.R[ind]+.1,labels = '*',cex = 1.5)
 dev.off()
 
 ################################
-# plot last coded coded exon chr18:69682683-69682823, e672980
+# plot last coded coded exon chr18:69683077-69684528, e672980
 tcf4Exon = 2^cleaningY(yExon[rownames(outExon),],modExon, P = 4)-1
-pd$ExonExpr = tcf4Exon['e672980',]
-pd$fdr = outExon['e672980',c('padj')]
+pd$ExonExpr = tcf4Exon['e673537',]
+pd$fdr = outExon['e673537',c('padj')]
 
-pdf("tcf4_mouse/plots/fig1_tcf4_last_exon.pdf",height = 3.5, width = 4)
+pdf("tcf4_mouse/plots/fig1_tcf4_last_exon.pdf",height = 4.5, width = 4)
 ggplot(data=pd) +
   geom_boxplot(aes(x=Age, y=ExonExpr, fill=Genotype),position = position_dodge(width = .5),outlier.shape=NA) +
   theme_bw(base_size = 14, base_family = "Helvetica")  + 
   geom_point(pch=21,aes(x=Age, y=ExonExpr, fill=Genotype), position=position_jitterdodge(dodge.width = .5))+
   scale_fill_manual(values = c("gray","red"),name = "",breaks = c("WT","HT")) +
   scale_x_discrete(labels =c("P1","P21","Adult")) + 
-  ylab("Adjusted Counts | SVs")+xlab("") + theme(axis.text.x=element_text(angle=45,hjust = 1)) +
-  theme(axis.title.y = element_text(family="Helvetica", face="bold", size=16),
-        axis.text.y = element_text(family="Helvetica", face="bold", size=14),
-        axis.text.x = element_text(family="Helvetica", face="bold", size=14))
+  ylab("Adjusted Counts | SVs")+xlab("") + 
+  theme(axis.title.y = element_text(family="Helvetica", face="bold", size=14),
+        axis.text.y = element_text(family="Helvetica", face="bold", size=12),
+        axis.text.x = element_text(family="Helvetica", face="bold", size=12),
+        legend.position="none")
 dev.off()
