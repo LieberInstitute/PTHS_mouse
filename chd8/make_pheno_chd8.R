4ea014b9bf72a973f18c29cafd16fdbcdf5e1e4f
badoi <badoi@me.com>
2017-02-15 11:51:18 -0500

    add chd8 DESeq2 analysis

--- chd8/make_pheno_chd8.R
+++ chd8/make_pheno_chd8.R
@@ -14,17 +14,26 @@ table(file.exists(pd$FASTQ.bz2))
 with(pd[!duplicated(pd$Sample_s),],table(Age,Genotype))
 # N of 1 for each age/genotype
 
-save(pd,file = 'rdas/pheno_chd8.rda')
-
-#make manifest file
+# change bz2 to gz
 thecall = with(pd, paste('bunzip2 -c <',FASTQ.bz2,'| gzip -c > ',FASTQ.gz))
 #parallel::mclapply(thecall,system, mc.cores = 36)
 table(file.exists(pd$FASTQ.gz))
 
-manifest = with(pd,paste(FASTQ.gz, 0, Sample_s,sep = '\t'))
+#merged fastq files
+
+pd = pd[!duplicated(pd$Sample_s),c("TITLE","Sample_s",
+                                   "Age", "Genotype", "Expr_Type")]
+pd$FASTQ = paste0(dir,'/merged_fastq/',pd$Sample_s,'.fastq.gz')
+table(file.exists(pd$FASTQ))
+
+save(pd,file = 'rdas/pheno_chd8.rda')
+
+#make manifest file
+
+manifest = with(pd,paste(FASTQ, 0, Sample_s,sep = '\t'))
 
 cat(manifest, file = paste0(dir,'/samples.manifest'),sep = '\n')
 
 ##########################
 # run the RNA seq pipeline
-# /dcl01/lieber/ajaffe/Emily/RNAseq-pipeline/sh/rnaseq-run-all.sh --experiment chd8 --prefix feb6 --reference mm10 --stranded true --ercc FALSE --cores 8 --large FALSE --fullcov FALSE
+# /dcl01/lieber/ajaffe/Emily/RNAseq-pipeline/sh/rnaseq-run-all.sh --experiment chd8 --prefix feb7 --reference mm10 --stranded "TRUE" --ercc "FALSE" --cores 8 --large "TRUE" --fullcov "FALSE"
